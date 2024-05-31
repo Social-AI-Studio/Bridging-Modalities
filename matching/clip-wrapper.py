546f10ac8841b77f0323ebd33b3e2cbde619c6db
Aditi Kumaresan <codetechie11@gmail.com>
2024-05-31 12:26:59 +0800

    modify annotations reading

--- matching/clip-wrapper.py
+++ matching/clip-wrapper.py
@@ -4,11 +4,14 @@ import clip
 import argparse
 
 import pandas as pd
-
+import json
 
 from PIL import Image
 
 def clip_similarity(image_path1, image_path2):
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, preprocess = clip.load("ViT-B/32", device=device)
 
     image1_preprocess = preprocess(Image.open(image_path1)).unsqueeze(0).to(device)
     image1_features = model.encode_image(image1_preprocess)
@@ -16,25 +19,32 @@ def clip_similarity(image_path1, image_path2):
     image2_preprocess = preprocess(Image.open(image_path2)).unsqueeze(0).to(device)
     image2_features = model.encode_image(image2_preprocess)
 
+    cos = torch.nn.CosineSimilarity(dim=0)
     similarity = cos(image1_features[0], image2_features[0]).item()
     similarity = (similarity + 1) / 2
     
     return similarity
 
 def main(annotation_filepath, img_folder,  output_folder):
-    df = pd.read_json(annotation_filepath, lines=True)
+    # df = pd.read_json(annotation_filepath, lines=True)
     os.makedirs(output_folder, exist_ok=True)
 
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device)
 
-    for id in df['id']:
+    data_list = []
+    with open(annotation_filepath, 'r', encoding='utf-8') as file:
+        for line in file:
+            record = json.loads(line)
+            data_list.append(record)
+
+    for record in data_list:
+        id = record["id"]
         img_filepath = os.path.join(img_folder, f"{id}.jpg")
         img = Image.open(img_filepath) 
 
         img_preprocess = preprocess(img).unsqueeze(0).to(device)
         img_features = model.encode_image(img_preprocess)
-        exit()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Converting Interpretations to Graph")
