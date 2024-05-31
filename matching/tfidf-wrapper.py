ea86415f428ed7b4f3bdddd8ddf05ee466a6cff1
Aditi Kumaresan <codetechie11@gmail.com>
2024-06-01 00:54:09 +0800

    fix similarity calculation

--- matching/tfidf-wrapper.py
+++ matching/tfidf-wrapper.py
@@ -3,16 +3,6 @@ import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 
-def get_top_k_similar(sim_matrix, labels, k):
-    top_k_similar = []
-    
-    for idx, label in zip(range(sim_matrix.shape[0]), labels):
-        similar_indices = sim_matrix[idx].argsort()[-(k+1):-1][::-1]
-        similar_scores = sim_matrix[idx][similar_indices]
-        top_k_similar.append(list(zip(label, similar_indices, similar_scores)))
-    
-    return top_k_similar
-
 def tfidf_similarity(query, corpus):
     # query -  str: "abcdef"
     # corpus - list: ["def", "ghi"]
@@ -26,16 +16,26 @@ def tfidf_similarity(query, corpus):
     
     # Calculate the cosine similarity between the two vectors
     # (1 x 2)
-    return cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:])
+    cosine_similarities = cosine_similarity(tfidf_matrix[0:1], tfidf_matrix[1:]).flatten()
+    
+    return cosine_similarities
+
+def get_top_k_similar(sim_matrix, labels, k):
+
+    top_k_indices = sim_matrix.argsort()[-k:][::-1]
+    top_k_scores = sim_matrix[top_k_indices]
+    top_k_labels = [labels[i] for i in top_k_indices]
+    
+    return list(zip(top_k_labels, top_k_indices, top_k_scores))
 
 def main(annotation_filepath):
     df = pd.read_json(annotation_filepath, lines=True)
     corpus = df['mistral_instruct_statement'].tolist()
     classes = df['class'].tolist()
 
-    sim_matrix = tfidf_similarity(corpus)
-    sim_matrix = get_top_k_similar(sim_matrix, classes, 4)
-    print(sim_matrix)
+    sim_matrix = tfidf_similarity("What is my name", corpus)
+    similar_entries = get_top_k_similar(sim_matrix, classes, 4)
+    print(similar_entries)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("Converting Interpretations to Graph")
