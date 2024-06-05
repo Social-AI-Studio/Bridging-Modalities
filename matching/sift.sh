88f5aaa82638ba8f2e430d71446cb62800c74273
Aditi Kumaresan <codetechie11@gmail.com>
2024-06-05 11:04:00 +0800

    sift features gen and sim matrix file

--- matching/sift.sh
+++ matching/sift.sh
@@ -1,4 +1,14 @@
+LATENT_HATRED=/mnt/data1/datasets/hatespeech/latent_hatred/truncated/explanations/train-explanations.jsonl
+MMHS=/mnt/data1/datasets/temp/MMHS150K/explanations/train-explanations.jsonl
+MISOGYNISTIC_MEME=/mnt/data1/datasets/memes/Misogynistic_MEME/annotations/explanation.jsonl
+
 python3 sift_wrapper.py \
-    --annotation_filepath /mnt/data1/datasets/temp/MMHS150K/annotations/train_truncated.jsonl \
-    --img_folder /mnt/data1/datasets/temp/MMHS150KTOTAL/img_resized \
-    --output_folder /mnt/data1/datasets/memes/cmtl-rag/mmhs/embeddings/sift/
+    --annotation_filepath /mnt/data1/datasets/memes/fhm_finegrained/annotations/dev_seen.json \
+    --caption_dir /mnt/data1/datasets/memes/fhm/captions/img_clean/blip2-opt-6.7b-coco/ \
+    --feature_dir /mnt/data1/datasets/memes/cmtl-rag/fhm/embeddings/sift \
+    --image_dir /mnt/data1/datasets/memes/fhm/images/img \
+    --support_filepaths $LATENT_HATRED $MISOGYNISTIC_MEME \
+    --support_caption_dirs "" /mnt/data1/datasets/memes/Misogynistic_MEME/preprocessing/blip2_captions/combined \
+    --support_feature_dirs "" /mnt/data1/datasets/memes/cmtl-rag/Misogynistic_MEME/embeddings/sift \
+    --support_image_dirs "" /mnt/data1/datasets/memes/Misogynistic_MEME/images/img_clean/combined \
+    --output_filepath /mnt/data1/datasets/memes/cmtl-rag/sim_matrices/fhm_sift_Misogynistic_MEME_matching.npy
