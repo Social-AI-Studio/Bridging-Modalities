0bc46792be073a8c3469b68febe4b27be2604479
mingshanhee <mingshan.hee@gmail.com>
2024-06-14 03:44:03 +0800

    Updated script for completeness

--- matching/tfidf.sh
+++ matching/tfidf.sh
@@ -2,18 +2,6 @@ LATENT_HATRED=/mnt/data1/datasets/hatespeech/latent_hatred/projects/CMTL-RAG/ann
 MISOGYNISTIC_MEME=/mnt/data1/datasets/memes/Misogynistic_MEME/annotations/explanation.jsonl
 
 FHM=/mnt/data1/datasets/memes/fhm_finegrained/annotations/dev_seen.json
-FHM_CAPS=/mnt/data1/datasets/memes/fhm/captions/img_clean/ofa-large-caption/
-MAMI=/mnt/data1/datasets/memes/mami/annotations/test.jsonl
-MAMI_CAPS=/mnt/data1/datasets/memes/mami/captions/deepfillv2/test/ofa-large-caption/
-
-ALIGNMENT_MEME=/mnt/data1/datasets/memes/fhm_finegrained/projects/CMTL-RAG/annotations/alignment_rationales_32.jsonl
-ALIGNMENT_CAPTIONS=/mnt/data1/datasets/memes/fhm/captions/deepfillv2/ofa-large-caption
-
-# Construct TF-IDF Similarity Matrix (LH, Text)
-LATENT_HATRED=/mnt/data1/datasets/hatespeech/latent_hatred/projects/CMTL-RAG/annotations/annotations.jsonl
-MISOGYNISTIC_MEME=/mnt/data1/datasets/memes/Misogynistic_MEME/annotations/explanation.jsonl
-
-FHM=/mnt/data1/datasets/memes/fhm_finegrained/annotations/dev_seen.json
 FHM_CAPS=/mnt/data1/datasets/memes/fhm/captions/deepfillv2/ofa-large-caption/
 
 MAMI=/mnt/data1/datasets/memes/mami/annotations/test.jsonl
